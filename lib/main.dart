ef6a00111e450509d60ca9365dc6ac10c96a3220
Natalia <you@example.com>
2024-06-03 18:25:27 -0600

    homepage1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:faker/faker.dart';
 import 'login_page.dart';
 import 'patient_profile_page.dart';
 import 'user_profile_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +13,12 @@ class MyApp extends StatelessWidget {
   final String cedula;
   final String hospital;
 
-  const MyApp({Key? key, this.doctorName = 'Guadalupe Espinosa López', this.cedula = 'ABCD800724MDFRRR00', this.hospital = 'Centro Médico de Occidente'}) : super(key: key);
+  const MyApp({
+    Key? key,
+    this.doctorName = 'Guadalupe Espinosa López',
+    this.cedula = 'ABCD800724MDFRRR00',
+    this.hospital = 'Centro Médico de Occidente',
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +29,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(doctorName: doctorName, cedula: cedula, hospital: hospital),
+      home: HomeScreen(
+        doctorName: doctorName,
+        cedula: cedula,
+        hospital: hospital,
+      ),
     );
   }
 }
@@ -33,7 +43,12 @@ class HomeScreen extends StatefulWidget {
   final String cedula;
   final String hospital;
 
-  const HomeScreen({Key? key, required this.doctorName, required this.cedula, required this.hospital}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+    required this.doctorName,
+    required this.cedula,
+    required this.hospital,
+  }) : super(key: key);
 
   @override
   HomeScreenState createState() => HomeScreenState();
@@ -56,84 +71,138 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.blue[300],
+        backgroundColor: const Color(0xFFDDEDFF),
         title: Image.asset('assets/SPINELINK_Vec.png', height: 50),
       ),
       body: Row(
         children: [
-          buildSidePanel(context),
+          buildProfileWidget(context),
           buildPatientListSection(context),
         ],
       ),
     );
   }
 
-  Widget buildSidePanel(BuildContext context) {
+  Widget buildProfileWidget(BuildContext context) {
     return Expanded(
       flex: 3,
-      child: Container(
-        color: Colors.grey[200],
-        child: Column(
-          children: [
-            const Expanded(
-              child: Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, size: 60),
+      child: SingleChildScrollView(
+        child: Container(
+          color: Colors.white, // Page background color
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 250,
+                      height: 250,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: const Color(0xFF193A61),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: buildInfoBox(widget.doctorName, Colors.blue[100]!),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: buildInfoBox(widget.cedula, Colors.blue[100]!),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: buildInfoBox(widget.hospital, Colors.blue[100]!),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: ElevatedButton.icon(
-                onPressed: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const UserProfilePage()),
-                  );
-                  if (result != null) {
-                    setState(() {});
-                  }
-                },
-                icon: const Icon(Icons.settings),
-                label: const Text('Editar Perfil', style: TextStyle(fontSize: 16)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue[100],
-                  foregroundColor: Colors.black,
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.doctorName,
+                      style: const TextStyle(
+                        fontFamily: 'Readex Pro',
+                        fontSize: 30,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const Spacer(),
-            buildTextButton('Ayuda'),
-            buildTextButton('Términos y Condiciones'),
-            buildTextButton('Aviso de Privacidad'),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: const Text('Cerrar Sesión', style: TextStyle(fontSize: 16, color: Colors.red)),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 35, 0, 0),
+                child: buildInfoBox(' ${widget.cedula}'),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                child: buildInfoBox('${widget.hospital}'),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+                child: ElevatedButton.icon(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const UserProfilePage(),
+                      ),
+                    );
+                    if (result != null) {
+                      setState(() {});
+                    }
+                  },
+                  icon: const Icon(Icons.settings),
+                  label: const Text('Editar Perfil', style: TextStyle(fontSize: 16)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFDDEDFF),
+                    foregroundColor: const Color(0xFF193A61),
+                    elevation: 3,
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: const BorderSide(
+                        color: Color(0xFF193A61),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginPage()),
+                    );
+                  },
+                  icon: const Icon(Icons.exit_to_app, color: Colors.red),
+                  label: const Text('Cerrar Sesión', style: TextStyle(fontSize: 16, color: Colors.red)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFCDD2), // Light red background
+                    foregroundColor: Colors.red,
+                    elevation: 3,
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: const BorderSide(
+                        color: Colors.red,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -271,24 +340,39 @@ class HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget buildInfoBox(String text, Color backgroundColor) {
+  Widget buildInfoBox(String text) {
     return Container(
-      padding: const EdgeInsets.all(8),
+      width: MediaQuery.of(context).size.width * 0.25,
+      height: MediaQuery.of(context).size.height * 0.06,
       decoration: BoxDecoration(
-        color: backgroundColor,
-        border: Border.all(color: Colors.blue[300]!, width: 1),
-        borderRadius: BorderRadius.circular(5),
+        color: const Color(0xFFDDEDFF),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: const Color(0xFF193A61),
+          width: 2,
+        ),
       ),
-      child: Center(child: Text(text, style: const TextStyle(fontSize: 18))),
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontFamily: 'Readex Pro',
+            color: Color(0xFF193A61),
+            fontSize: 20,
+            letterSpacing: 0,
+          ),
+        ),
+      ),
     );
   }
 
-  Widget buildTextButton(String text) {
+  Widget buildTextButton(String text, Color color) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: TextButton(
         onPressed: () {},
-        child: Text(text, style: const TextStyle(fontSize: 16)),
+        child: Text(text, style: TextStyle(fontSize: 16, color: color)),
       ),
     );
   }
